9091200b04682cc295473b1b8daa8e228b8c1811
Lucasn24 <e1406319@u.nus.edu>
2025-10-31 00:09:15 +0800

    Fix delete

--- src/main/java/presspal/contact/logic/parser/DeleteCommandParser.java
+++ src/main/java/presspal/contact/logic/parser/DeleteCommandParser.java
@@ -5,7 +5,10 @@ import static presspal.contact.logic.parser.CliSyntax.PREFIX_INDEX;
 
 import presspal.contact.commons.core.index.Index;
 import presspal.contact.logic.commands.DeleteCommand;
+import presspal.contact.logic.commands.ListInterviewCommand;
 import presspal.contact.logic.parser.exceptions.ParseException;
+
+import java.util.stream.Stream;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -20,6 +23,11 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     public DeleteCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_INDEX);
+
+        if (!isPrefixPresent(argMultimap, PREFIX_INDEX) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
+
         Index index;
         try {
             index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_INDEX).get());
@@ -28,6 +36,14 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
         }
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean isPrefixPresent(ArgumentMultimap argumentMultimap, Prefix prefix) {
+        return Stream.of(prefix).allMatch(p -> argumentMultimap.getValue(p).isPresent());
     }
 
 }
